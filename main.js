c272f80ab3391dd351b00fc61862f39753348b5f
Mohamed Rezk <96630126+mohamedrezk98@users.noreply.github.com>
2023-07-15 19:47:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let img=document.getElementsByClassName('.img'); 
+let container=document.querySelector('.container'); 
+
+function phones(phone){
+    
+    img.src=phone;
+    
+    
+}
+function back(color){
+    container.style.background=color;
+    
+}
